3fcbe3d99e2983f72429765cee800547fe6211dd
Todd H <github@simplybest.com>
2024-10-15 22:28:38 -0600

    layout home page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,101 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
+    <ion-header>
+      <ion-toolbar color="primary">
+        <ion-title>
+          Topzee
+        </ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-content class="ion-padding" :scrollY="false">
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <ion-card top-card>
+        <ion-card-content top-content>
+          <ion-img src="/assets/logo dice.png" style="height:100px;margin-bottom:10px"
+            alt="topzee logo: two green dice"></ion-img>
+          <ion-grid>
+            <ion-row v-if="gameService.lifetimeStats.lastGame" bold>
+              <ion-col>Last game</ion-col>
+              <ion-col class="ion-text-end">{{ gameService.lifetimeStats.lastGame }}
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col>Games played</ion-col>
+              <ion-col class="ion-text-end">{{ gameService.lifetimeStats.completedGameCount }}</ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col>Average</ion-col>
+              <ion-col class="ion-text-end">{{ gameService.lifetimeStats.averageScore }}</ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col>High Score</ion-col>
+              <ion-col class="ion-text-end">🏆 {{ gameService.lifetimeStats.highScore }}</ion-col>
+            </ion-row>
+          </ion-grid>
+        </ion-card-content>
+      </ion-card>
+
+      <ion-button expand="block" v-if="!gameService.game?.playing" @click="playGame()">
+        <ion-icon slot="start" :icon="add" color="success"></ion-icon>
+        Start Game
+      </ion-button>
+      <ion-button expand="block" v-if="gameService.game?.playing" @click="playGame()">
+        <ion-icon slot="start" :icon="pauseCircleOutline" color="success"></ion-icon>
+        Resume Game ({{ gameService.game?.turnsLeft }} turns left)
+      </ion-button>
+      <ion-button expand="block" v-if="gameService.game?.playing" @click="restartGame()" color="light" fill="solid">
+        Restart Game
+      </ion-button>
+      
     </ion-content>
+
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon, IonCard, IonCardContent, IonGrid, IonRow, IonCol, IonImg } from '@ionic/vue';
+import { pauseCircleOutline, add } from 'ionicons/icons';
+import { reactive } from 'vue';
+
+const gameService = reactive({
+  lifetimeStats: {
+    lastGame: '2021-09-01',
+    completedGameCount: 10,
+    averageScore: 10.5,
+    highScore: 20
+  },
+  game: {
+    playing: false,
+    turnsLeft: 3
+  }
+});
+
+const playGame = () => {
+  gameService.game.playing = true
+}
+
+const restartGame = () => {
+  gameService.game.playing = false
+}
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+ion-card {
+    margin: 0 0 16px;
+    --color: black;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+ion-button + ion-button {
+    margin-top: 16px;
 }
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
+ion-card-content {
+    padding: 12px;
+    font-size: 2.5vh;
 }
 
-#container a {
-  text-decoration: none;
+[bold] {
+    font-weight: bold;
 }
 </style>
